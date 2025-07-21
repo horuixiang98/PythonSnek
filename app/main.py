4c0611b83e54fa35cc9730bc9b3de0550abb65dc
horuixiang <horuixiang@gmail.com>
2025-07-21 18:09:28 +0800

    add math

--- app/main.py
+++ app/main.py
@@ -4,21 +4,33 @@ import asyncio
 from typing import Dict, List
 import platform
 import oneagent
+import math
 
 # Detect Python implementation (eg. CPython)
 print(platform.python_implementation())
 
-# # Python OneAgent SDK
-# if not oneagent.initialize():
-#     print('Error initializing OneAgent SDK.')
+description = """
+Python Snek API helps you do awesome stuff. 🚀
+"""
 
-# with oneagent.get_sdk().trace_incoming_remote_call('method', 'service', 'endpoint'):
-#     pass
+tags_metadata = [
+    {
+        "name": "OneAgent Python SDK",
+        "description": "OneAgent Python SDK initialization",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://github.com/Dynatrace/OneAgent-SDK-for-Python",
+        },
+    },
+]
 
-# print('It may take a few moments before the path appears in the UI.')
-# input('Please wait...')
-
-app = FastAPI()
+app = FastAPI(
+        title="Python Snek App",
+        description=description,
+        summary="This is a backend test API for Python Snek Testing",
+        version="0.0.1",
+        openapi_tags=tags_metadata,
+    )
 
 @app.get("/")
 async def read_root():
@@ -28,8 +40,32 @@ async def read_root():
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/oneagentsdk/init", tags=["OneAgent Python SDK"])
+async def initOneAgentSDK():
+    init_result = oneagent.initialize()
+    # print('OneAgent SDK initialization result' + repr(init_result))
+    if init_result:
+        return 'SDK should work (but agent might be inactive).'
+    else:
+        return 'SDK will definitely not work (i.e. functions will be no-ops):', init_result
+
+@app.get("/oneagentsdk/testtrace", tags=["OneAgent Python SDK"])
+async def testTrace():
+    with oneagent.get_sdk().trace_incoming_remote_call('method', 'service', 'endpoint'):
+        pass
+    # Perform some complex mathematical operations
+    result = 0
+    for i in range(1000):
+        result += (i ** 2) * (math.sin(i) + math.cos(i))
+    # Simulate heavy processing with sleep
+        await asyncio.sleep(2)
+        return result
+    
+    
+
+
 # Store intervals data in memory
-intervals_data: Dict[str, List[Dict]] = {}
+# intervals_data: Dict[str, List[Dict]] = {}
 
 # @app.post("/intervals/create")
 # async def create_interval(start: int, end: int, step: int = 1):
