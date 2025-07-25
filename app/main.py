e182a031f495c59f2740cac80790c1a505050cb5
horuixiang <horuixiang@gmail.com>
2025-07-25 13:34:51 +0800

    -

--- app/main.py
+++ app/main.py
@@ -14,8 +14,6 @@ async def startup_event():
     with sdk.trace_custom_service('PythonSnekApp', 'PythonSnekService'):
         print('do some fancy stuff')
 
-
-
 # Middleware to trace all incoming requests
 @app.middleware("http")
 async def add_dynatrace_trace(request: Request, call_next):
@@ -73,3 +71,43 @@ async def health_check():
         time.sleep(0.01)
         tracer.end()    
         return {"status": "healthy"}
+
+@app.get("/mock_incoming_web_request")
+def mock_incoming_web_request():
+    sdk = getsdk()
+    wappinfo = sdk.create_web_application_info(
+        virtual_host='example.com', # Logical name of the host server.
+        application_id='MyWebApplication', # Unique web application ID.
+        context_root='/my-web-app/') # App's prefix of the path part of the URL.
+
+    with wappinfo:
+        # This with-block will automatically free web application info handle
+        # at the end. Note that the handle can be used for multiple tracers. In
+        # general, it is recommended to reuse web application info handles as
+        # often as possible (for efficiency reasons). For example, if you use
+        # WSGI, the web application info could be stored as an attribute of the
+        # application object.
+        #
+        # Note that different ways to specify headers, response headers and
+        # parameter (form fields) not shown here also exist. Consult the
+        # documentation for trace_incoming_web_request and
+        # IncomingWebRequestTracer.
+        wreq = sdk.trace_incoming_web_request(
+            wappinfo,
+            'http://example.com/my-web-app/foo?bar=baz',
+            'GET',
+            headers={'Host': 'example.com', 'X-foo': 'bar'},
+            remote_address='127.0.0.1:12345')
+        with wreq:
+            wreq.add_parameter('my_form_field', '1234')
+            # Process web request
+            wreq.add_response_headers({'Content-Length': '1234'})
+            wreq.set_status_code(200) # OK
+
+            # Add 3 different custom attributes.
+            sdk.add_custom_request_attribute('custom int attribute', 42)
+            sdk.add_custom_request_attribute('custom float attribute', 1.778)
+            sdk.add_custom_request_attribute('custom string attribute', 'snow is falling')
+
+            # This call will trigger the diagnostic callback.
+            sdk.add_custom_request_attribute('another key', None)
