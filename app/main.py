8ed4907233b990dbcd6833ae4841e140f9b104ae
horuixiang <horuixiang@gmail.com>
2025-07-31 17:57:14 +0800

    -

--- app/main.py
+++ app/main.py
@@ -164,8 +164,11 @@ def mock_outgoing_web_request():
         outgoing_remote_call(success=False)
 
         link = sdk.create_in_process_link()
+        payload = {
+            "link": link,
+        }
         try:
-            response = requests.post('http://localhost:8000/mock_incoming_web_request', link=link)
+            response = requests.post('http://localhost:8000/mock_incoming_web_request', payload=payload)
             if response.status_code == 200:
                 print("Successfully called mock_incoming_web_request")
             else:
