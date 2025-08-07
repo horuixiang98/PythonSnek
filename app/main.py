7cd02f347fb5147a82e3e856b269d3970e7fb8d9
horuixiang <horuixiang@gmail.com>
2025-08-07 14:42:41 +0800

    -

--- app/main.py
+++ app/main.py
@@ -161,7 +161,7 @@ def mock_outgoing_web_request(request: Request):
         tracer.add_response_headers({'Content-Length': '1234'})
         tracer.set_status_code(200) # OK
         outgoing_remote_call(success=True)
-        rcall = outgoing_remote_call(success=True)
+        outgoing_remote_call(success=True)
         outgoing_remote_call(success=False)
         try:
             response = requests.post('http://localhost:8000/mock_incoming_web_request', params={'link': tag})
