7eed8cbccc52ff62e739f02149eaa095761f4ed6
horuixiang <horuixiang@gmail.com>
2025-09-03 17:25:34 +0800

    -

--- app/plus_demo/plus_lane.py
+++ app/plus_demo/plus_lane.py
@@ -43,33 +43,30 @@ def mock_outgoing_web_request(request: Request):
             # Do actual DB request
             tracer.set_rows_returned(42) # Optional
             tracer.set_round_trip_count(3) # Optional 
-        call = getsdk().trace_outgoing_remote_call(
-            'ScannerPyMethod', 'ScannerPyService', 'dupypr://plus-demo.com/ScannerEndpoint',
-            onesdk.Channel(onesdk.ChannelType.IN_PROCESS, 'plus-demo.com'),
-            protocol_name='Scanner_PY_PROTOCOL')
-        with call:
-                # Note that this property can only be accessed after starting the
-                # tracer. See the documentation on tagging for more information.
-                strtag = call.outgoing_dynatrace_string_tag
-                do_remote_call_thread_func(strtag, success=True)
-                # deductCredit = sdk.trace_incoming_remote_call(
-                #     'deductCreditMethod', 'deductCreditServiceName', 'rmi://plus-demo.com/ScannerEndpoint/deductCredit',
-                #     protocol_name='RMI/custom',
-                #     str_tag=strtag)
-                # with deductCredit:
-                #     do_remote_call_thread_func(strtag, True)
-                #     pass # Here you would do the actual work that is timed
+            call = getsdk().trace_outgoing_remote_call(
+                'ScannerPyMethod', 'ScannerPyService', 'dupypr://plus-demo.com/ScannerEndpoint',
+                onesdk.Channel(onesdk.ChannelType.IN_PROCESS, 'plus-demo.com'),
+                protocol_name='Scanner_PY_PROTOCOL')
+            with call:
+                    # Note that this property can only be accessed after starting the
+                    # tracer. See the documentation on tagging for more information.
+                    strtag = call.outgoing_dynatrace_string_tag
+                    do_remote_call_thread_func(strtag, success=True)
+                    # deductCredit = sdk.trace_incoming_remote_call(
+                    #     'deductCreditMethod', 'deductCreditServiceName', 'rmi://plus-demo.com/ScannerEndpoint/deductCredit',
+                    #     protocol_name='RMI/custom',
+                    #     str_tag=strtag)
+                    # with deductCredit:
+                    #     do_remote_call_thread_func(strtag, True)
+                    #     pass # Here you would do the actual work that is timed
 
 
 def do_remote_call_thread_func(strtag, success):
     try:
-        print('+thread')
-        # We use positional arguments to specify required values and named
-        # arguments to specify optional values.
         incall = getsdk().trace_incoming_remote_call(
-            'dummyPyMethod', 'DummyPyService',
-            'dupypr://localhost/dummyEndpoint',
-            protocol_name='DUMMY_PY_PROTOCOL', str_tag=strtag)
+            'deductCreditMethod', 'deductCreditService',
+            'rmi://plus-demo.com/ScannerEndpoint/deductCredit',
+             protocol_name='RMI/custom', str_tag=strtag)
         with incall:
             if not success:
                 raise RuntimeError('Remote call failed on the server side.')
@@ -77,10 +74,6 @@ def do_remote_call_thread_func(strtag, success):
                 'Northwind', onesdk.DatabaseVendor.SQLSERVER,
                 onesdk.Channel(onesdk.ChannelType.TCP_IP, '10.0.0.42:6666'))
 
-            # This with-block will automatically free the database info handle
-            # at the end. Note that the handle is used for multiple tracers. In
-            # general, it is recommended to reuse database (and web application)
-            # info handles as often as possible (for efficiency reasons).
             with dbinfo:
                 traced_db_operation(
                     dbinfo, "BEGIN TRAN;")
@@ -94,7 +87,6 @@ def do_remote_call_thread_func(strtag, success):
                     dbinfo,
                     "UPDATE baz SET foo = foo + 1 WHERE qux = 23;")
                 traced_db_operation(dbinfo, "COMMIT;")
-        print('-thread')
     except Exception as e:
         raise
 
