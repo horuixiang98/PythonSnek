46e640d558f10f4b3470890e320f97964aa11584
horuixiang <horuixiang@gmail.com>
2025-09-04 11:40:11 +0800

    Functions

--- app/plus_demo/plus_lane.py
+++ app/plus_demo/plus_lane.py
@@ -10,15 +10,22 @@ app = FastAPI()
 sdk = oneagent.get_sdk()
 getsdk = oneagent.get_sdk
 
+class TraceObject:
+    def __init__(self, method: str, service: str, endpoint: str, protocol: str):
+        self.Method = method
+        self.Service = service
+        self.Endpoint = endpoint
+        self.Protocol = protocol
+
 # Check SDK status at startup
 @app.on_event("startup")
 async def startup_event():
     init_result = oneagent.initialize()
     print('OneAgent SDK initialization result' + repr(init_result))
     with sdk.trace_custom_service('PythonSnekApp', 'PythonSnekService'):
-        print('do some fancy stuff')
+        print('Application initialized')
 
-
+##################################### API #######################################
 @app.get("/plus_lane_one")
 def mock_outgoing_web_request(request: Request):
     sdk = getsdk()
@@ -44,26 +51,66 @@ def mock_outgoing_web_request(request: Request):
             tracer.set_rows_returned(42) # Optional
             tracer.set_round_trip_count(3) # Optional 
             with tracer:
-                call = getsdk().trace_outgoing_remote_call(
-                    'ScannerPyMethod', 'ScannerPyService', 'dupypr://plus-demo.com/ScannerEndpoint',
-                    onesdk.Channel(onesdk.ChannelType.IN_PROCESS, 'plus-demo.com'),
-                    protocol_name='Scanner_PY_PROTOCOL')
-                with call:
-                        # Note that this property can only be accessed after starting the
-                        # tracer. See the documentation on tagging for more information.
-                        strtag = call.outgoing_dynatrace_string_tag
-                        do_remote_call(strtag, success=True)
+                traceInfo = TraceObject('ScannerPyMethod', 'ScannerPyService', 'dupypr://plus-demo.com/ScannerEndpoint', 'Scanner_PY_PROTOCOL')
+                traceTag = trace_outgoing_remote_call_func(traceInfo)
+                with traceTag:
+                        do_remote_call(traceTag, success=True)
 
+
+##################################### Functions #######################################
+def trace_outgoing_remote_call_func(trace_obj: TraceObject):
+    call = getsdk().trace_outgoing_remote_call(
+                    trace_obj.Method, trace_obj.Service, trace_obj.Endpoint,
+                    onesdk.Channel(onesdk.ChannelType.IN_PROCESS, trace_obj.Endpoint),
+                    protocol_name=trace_obj.Protocol)
+    with call:
+            strtag = call.outgoing_dynatrace_string_tag
+            return strtag
+
+def trace_incoming_remote_call_func(strtag, success, trace_obj: TraceObject):
+    incall = getsdk().trace_incoming_remote_call(
+            trace_obj.Method, trace_obj.Service,
+            trace_obj.Endpoint,
+            protocol_name=trace_obj.Protocol, str_tag=strtag)
+
+    with incall:
+        if not success:
+            raise RuntimeError('Remote call failed on the server side.')
+        return incall
+
+
+
+def traced_db_operation(dbinfo, sql):
+
+    # Entering the with block automatically start the tracer.
+    with getsdk().trace_sql_database_request(dbinfo, sql) as tracer:
+
+        # In real-world code, you would do the actual database operation here,
+        # i.e. call the database's API.
+
+        # Set an optional "exit"-field on the tracer. Whenever there is a
+        # setter available on a tracer (as opposed to an optional parameter to a
+        # trace_* function), it may be called anytime between creating and
+        # ending the tracer (i.e. also after starting it).
+        tracer.set_round_trip_count(3)
+
+    print('-db', dbinfo, sql)
+
+
+def do_remote_call(strtag, success):
+    workerthread = threading.Thread(
+        target=do_remote_call_thread_func,
+        args=(strtag, success))
+    workerthread.start()
+    # Note that we need to join the thread, as all tagging assumes synchronous
+    # calls.
+    workerthread.join()
 
 def do_remote_call_thread_func(strtag, success):
     try:
-        incall = getsdk().trace_incoming_remote_call(
-            'deductCreditMethod', 'deductCreditService',
-            'dupypr://plus-demo.com/ScannerEndpoint',
-            protocol_name='RMI/custom', str_tag=strtag)
+        traceInfo = TraceObject('deductCreditMethod', 'deductCreditService', 'dupypr://plus-demo.com/ScannerEndpoint', 'RMI/custom')
+        incall = trace_incoming_remote_call_func(strtag, success, traceInfo)
         with incall:
-            if not success:
-                raise RuntimeError('Remote call failed on the server side.')
             dbinfoDeductCredit = getsdk().create_database_info(
                 'deductCredit', onesdk.DatabaseVendor.SQLSERVER,
                 onesdk.Channel(onesdk.ChannelType.TCP_IP, '10.0.0.42:6666'))
@@ -83,33 +130,3 @@ def do_remote_call_thread_func(strtag, success):
                 traced_db_operation(dbinfoDeductCredit, "COMMIT;")
     except Exception as e:
         raise
-
-def do_remote_call(strtag, success):
-    # This function simulates doing a remote call by calling a function
-    # do_remote_call_thread_func in another thread, passing a string tag. See
-    # the documentation on tagging for more information.
-    workerthread = threading.Thread(
-        target=do_remote_call_thread_func,
-        args=(strtag, success))
-    workerthread.start()
-
-    # Note that we need to join the thread, as all tagging assumes synchronous
-    # calls.
-    workerthread.join()
-
-def traced_db_operation(dbinfo, sql):
-    print('+db', dbinfo, sql)
-
-    # Entering the with block automatically start the tracer.
-    with getsdk().trace_sql_database_request(dbinfo, sql) as tracer:
-
-        # In real-world code, you would do the actual database operation here,
-        # i.e. call the database's API.
-
-        # Set an optional "exit"-field on the tracer. Whenever there is a
-        # setter available on a tracer (as opposed to an optional parameter to a
-        # trace_* function), it may be called anytime between creating and
-        # ending the tracer (i.e. also after starting it).
-        tracer.set_round_trip_count(3)
-
-    print('-db', dbinfo, sql)
